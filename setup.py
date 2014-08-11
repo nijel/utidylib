167db358c314b1ebb83610f35dd09f7a08861ebb
Michal Čihař <michal@cihar.com>
2014-08-11 15:46:43 +0200

    Remove unused imports
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from distutils.core import setup
 from setuptools.command.install import install
-from distutils.command.install_data import install_data
-from distutils.command.bdist_wininst import bdist_wininst
 
 
 class install_utidylib(install):
