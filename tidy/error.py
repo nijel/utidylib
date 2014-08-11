d7387901def3385b3eef350379c0e90c5c1bacb4
Michal Čihař <michal@cihar.com>
2014-08-11 15:18:17 +0200

    Add docblocks
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- tidy/error.py
+++ tidy/error.py
@@ -1,20 +1,33 @@
+"""
+Exceptions for uTidyLib
+"""
+
 __all__ = (
     'TidyLibError', 'InvalidOptionError', 'OptionArgError',
 )
 
 
 class TidyLibError(Exception):
+    """
+    Generic Tidy exception.
+    """
     def __init__(self, arg):
         self.arg = arg
 
 
 class InvalidOptionError(TidyLibError):
+    """
+    Exception for invalid option.
+    """
     def __str__(self):
         return "%s was not a valid Tidy option." % (self.arg)
     __repr__ = __str__
 
 
 class OptionArgError(TidyLibError):
+    """
+    Exception for invalid parameter.
+    """
     def __init__(self, arg):
         self.arg = arg
 
