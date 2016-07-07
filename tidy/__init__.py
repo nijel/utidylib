c2a055f4031791ea544e48b5a4c45c5b5e7b8c89
Michal Čihař <michal@cihar.com>
2016-07-07 13:58:57 +0200

    Set version to 0.4
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- tidy/__init__.py
+++ tidy/__init__.py
@@ -45,4 +45,4 @@ __all__ = [
     'error', 'lib', 'parse', 'parseString', 'Document', 'ReportItem',
     'TidyLibError', 'InvalidOptionError', 'OptionArgError',
 ]
-__version__ = '0.3'
+__version__ = '0.4'
