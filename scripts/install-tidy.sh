8e45e85e2531907e20ce50d46aeab9c362d2a995
Michal Čihař <michal@cihar.com>
2022-04-26 11:57:49 +0200

    Test with native tidy as well

--- scripts/install-tidy.sh
+++ scripts/install-tidy.sh
@@ -7,6 +7,15 @@ if [ -z "$1" ] ; then
     exit 1
 fi
 
+if which apt-get ; then
+  if [ "$1" = "os" ] ; then
+    sudo apt-get install -y libtidy5deb1
+    exit 0
+  else
+    sudo apt-get purge libtidy5deb1 tidy libtidy-dev
+  fi
+fi
+
 wget -O tidy.tar.gz https://github.com/htacg/tidy-html5/archive/$1.tar.gz
 mkdir tidy-source
 tar xvf tidy.tar.gz --strip-components=1 -C tidy-source
