94b40402906d816bcaab3bbce4d19a63eaf64568
Michal Čihař <michal@cihar.com>
2022-04-26 12:50:23 +0200

    ci: Fix macos installation

--- scripts/install-tidy.sh
+++ scripts/install-tidy.sh
@@ -7,6 +7,7 @@ if [ -z "$1" ] ; then
     exit 1
 fi
 
+CMAKE_ARGS=""
 if which apt-get ; then
   if [ "$1" = "os" ] ; then
     sudo apt-get install -y libtidy5deb1
@@ -14,6 +15,7 @@ if which apt-get ; then
   else
     sudo apt-get purge libtidy5deb1 tidy libtidy-dev
   fi
+  CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX=/usr"
 fi
 
 wget -O tidy.tar.gz https://github.com/htacg/tidy-html5/archive/$1.tar.gz
@@ -21,6 +23,6 @@ mkdir tidy-source
 tar xvf tidy.tar.gz --strip-components=1 -C tidy-source
 rm tidy.tar.gz
 cd tidy-source/build/cmake/
-cmake ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+cmake ../.. -DCMAKE_BUILD_TYPE=Release $CMAKE_ARGS
 make
 sudo make install
